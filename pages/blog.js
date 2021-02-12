f56bc1b6bdd052260f513e6015948a869de0c32c
Renan Gabriel <renanthompsom@gmail.com>
2021-02-12 10:04:49 -0300

    Update blog description

--- pages/blog.js
+++ pages/blog.js
@@ -18,7 +18,7 @@ export default function Blog({ posts }) {
   return (
     <Container
       title="Blog – Renan Gabriel"
-      description="Thoughts on the software industry, programming, tech, videography, music, and my personal life."
+      description="Blog sobre desenvolvimento web."
     >
       <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 w-full">
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
